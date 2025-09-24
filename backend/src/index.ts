fa9f7106f2bd9504f537e64a07fc0f15eb615e68
Amit Yadav <amity7205@gmail.com>
2025-09-24 11:59:36 +0530

    prisma init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { ENV } from "../config/env";
+import { router } from "../routes";
 
 const app=express();
 
+app.use(express.json());
 
+app.use("/api/v1/",router);
 app.listen(ENV.PORT,()=>{
     console.log(`Server is running on ${ENV.PORT}`);
-})
+});
