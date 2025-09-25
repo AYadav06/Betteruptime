b91af6b81d43a6bd2e7aa90e97fe2e9494598dab
Amit Yadav <amity7205@gmail.com>
2025-09-26 00:09:19 +0530

    add .env.example

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,8 @@ import { ENV } from "../config/env";
 import { router } from "../routes";
 import cookieParser from "cookie-parser";
 const app=express();
-
 app.use(express.json());
 app.use(cookieParser());
-
 app.use("/api/v1/",router);
 
 app.listen(ENV.PORT,()=>{
